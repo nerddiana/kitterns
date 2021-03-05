7a6cad103174483cbba88faf93ffa8ebab9ee95a
unknown <vichyvaleria@gmail.com>
2021-03-04 22:38:29 -0700

    cambiando index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="container">
+  <!-- <div class="container">
     <div>
-      <!--Logo />
-      <img src="@/assets/img/imagen.jpeg" />
-      <img src="https://cde.laprensa.e3.pe/ima/0/0/2/3/8/238082.jpg" /-->
       <template>
         <section>
           <div v-if="$device.isDesktop">Desktop</div>
@@ -11,10 +8,10 @@
           <div v-else>Mobile</div>
         </section>
       </template>
-      <h1 class="title">kitterns</h1>
+
       <h2>un subtitulo {{ $t('homePage.wellcome') }}</h2>
       <SelectLocale />
-      <Button label="Call To Acction (go to help)" to="/help" />
+      <Button label="Enviar" to="/help" />
       <p>Valor: {{ valor }}</p>
       <Input v-model="valor" placeholder="un input" />
       <div class="links">
@@ -36,14 +33,98 @@
         </a>
       </div>
     </div>
+  </div> -->
+  <div>
+    <section class="hero">
+      <div>
+        <h1 class="title">¡MESES SIN INTERESES!</h1>
+        <h2 class="subtitle">¡MESES SIN INTERESES!</h2>
+        <div class="btn-container">
+          <btn-small btn-text="productos" />
+        </div>
+      </div>
+    </section>
+
+    <section class="p-6">banner oferta</section>
+
+    <section class="p-6">
+      <!-- <search-bar /> -->
+      <!-- <productcard
+        prod-desc="culo culo culo culo culo culo culo culo Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni facere repellat eligendi eum."
+        prod-name="Nombre del producto"
+      /> -->
+    </section>
+
+    <template>
+      <section>
+        <!-- <sort-button btn-name="Precio" /> -->
+        <commentinput />
+      </section>
+    </template>
+
+    <template class="product-cards">
+      <section class="container">
+        <h2 class="mb-4 h2-b">¡OFERTAS!</h2>
+        <div class="overflow-x-scroll overflow-hidden pb-10 hide-scroll-bar">
+          <div class="flex flex-nowrap">
+            <div class="inline-block mr-6">
+              <productcard
+                prod-desc="culo culo culo culo culo culo culo culo Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni facere repellat eligendi eum."
+                prod-name="Nombre del producto"
+              />
+            </div>
+            <div class="inline-block mr-6">
+              <productcard
+                prod-desc="culo culo culo culo culo culo culo culo Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni facere repellat eligendi eum."
+                prod-name="Nombre del producto"
+              />
+            </div>
+            <div class="inline-block mr-6">
+              <productcard
+                prod-desc="culo culo culo culo culo culo culo culo Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni facere repellat eligendi eum."
+                prod-name="Nombre del producto"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </template>
+
+    <template>
+      <div class="">
+        <section class="container">
+          <h2 class="mb-4 h2-b">Categorías</h2>
+          <div class="overflow-x-scroll pb-10 hide-scroll-bar">
+            <div class="flex flex-nowrap">
+              <div class="inline-block mr-6">
+                <card-categorie card-text="Juegos y juguetes" />
+              </div>
+              <div class="inline-block mr-6">
+                <card-categorie card-text="Boxing kits" />
+              </div>
+              <div class="inline-block mr-6">
+                <card-categorie card-text="Especialidad" />
+              </div>
+              <div class="inline-block mr-6">
+                <card-categorie card-text="Accesorios" />
+              </div>
+              <div class="inline-block mr-6">
+                <card-categorie card-text="Productos de oficina" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
+import CardCategorie from '~/components/card-categorie.vue'
 export default {
   data() {
     return {
-      valor: '',
+      valor: CardCategorie,
     }
   },
   methods: {
@@ -57,27 +138,33 @@ export default {
 <style lang="scss">
 /* Sample `apply` at-rules with Tailwind CSS  */
 
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+.hero {
+  @apply flex bg-purple p-4 items-center justify-center align-middle;
+
+  min-height: 275px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+section.container {
+  @apply px-8 py-4;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+// .title {
+//   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+//     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+//   display: block;
+//   font-weight: 300;
+//   font-size: 100px;
+//   color: #35495e;
+//   letter-spacing: 1px;
+// }
+
+// .subtitle {
+//   font-weight: 300;
+//   font-size: 42px;
+//   color: #526488;
+//   word-spacing: 5px;
+//   padding-bottom: 15px;
+// }
 
 .links {
   padding-top: 15px;
