4c982c5e5cff9b952bed4dc073671d0a269d3b07
unknown <vichyvaleria@gmail.com>
2021-03-05 18:10:16 -0700

    datos de la cuenta terminada

--- pages/account.vue
+++ pages/account.vue
@@ -11,14 +11,49 @@
     </div>
     <!-- form -->
     <div class="mt-6 mb-8 w-full flex justify-start align-middle items-center">
-      <form class="max-w-lg" action="">
-        <div>
+      <form
+        class="flex flex-wrap content-center align-center max-w-lg"
+        action=""
+      >
+        <div class="space-y-10 mb-8 md:mr-4">
           <Input
             class="h-12 w-full flex content-center"
             :label="$t('contactForm.newPsw')"
             :placeholder="$t('contactForm.Psw')"
             type="password"
           />
+          <div class="flex justify-between space-x-2">
+            <div class="space-y-1">
+              <p class="text-alert text-xs">
+                {{ $t('access.lowercase') }}
+              </p>
+
+              <p class="text-alert text-xs">
+                {{ $t('access.uppercase') }}
+              </p>
+            </div>
+            <div>
+              <p class="text-alert text-xs">
+                {{ $t('access.character') }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="">
+          <div class="space-y-10">
+            <Input
+              :label="$t('contactForm.confirmNewPsw')"
+              :placeholder="$t('contactForm.confirmNewPsw')"
+              type="password"
+              class="h-12 flex content-center"
+            />
+            <p class="text-alert text-sm">
+              {{ $t('access.wrongPss') }}
+            </p>
+          </div>
+          <div class="flex mt-8">
+            <btn-primary class="w-full" :btn-value="$t('buttons.logIn')" />
+          </div>
         </div>
       </form>
     </div>
